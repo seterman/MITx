98dc419a53d0a03660e14bf945e3a5ae01290533
OpenShift guest <seterman@mit.edu>
2013-06-10 14:23:06 -0400

    basic calculator

--- day1.js
+++ day1.js
@@ -1,7 +1,88 @@
-function calculate()
+function calculate(text)
 {
-    var input = $('#text1:first'); // get input field from DOM
-    var val = input.val();
-    var output = $('#text1_out:first');
-    output.text(val);
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g;
+    var tokens = text.match(pattern);
+    
+    try
+    {
+        var answer = evaluate(tokens);
+        if(tokens.length !== 0){ throw "Ill-formed exception"; }
+        return String(answer);
+    }
+    catch(err)
+    {
+        return err;
+    }
 }
+
+function setup_calc(div)
+{
+    var input = $('<input></input>',{type:"text", size: 50});
+    var output  = $('<div></div>');
+    var button = $('<button>Calculate</button>');
+    button.bind("click", function()
+    {
+        output.text(String(calculate(input.val())));
+    });
+    
+    $(div).append(input,button,output);
+}
+
+function read_operand(tokens)
+{
+    var first = tokens[0];
+    tokens.shift();
+    var number = parseInt(first,10);
+    
+    if (isNaN(number))   
+    {
+       throw "Number expected";
+    }
+    else
+    {
+        return number;
+    }
+    
+}
+
+function evaluate(tokens)
+{
+    var operands = ['+','-','*','/'];
+    
+    if (tokens.length === 0)
+    {
+        throw "Missing operand";
+    }
+    else
+    {
+        var value = read_operand(tokens);
+        
+        while(tokens.length !== 0)
+        {
+            var op = tokens.shift();
+            if(operands.indexOf(op)==-1)
+            {
+                throw "Missing operand"
+            }
+            else
+            {
+                var value2 = read_operand(tokens);
+                if(op=='+'){ value= value + value2; }
+                if(op=='-'){ value= value - value2; }
+                if(op=='*'){ value= value * value2; }
+                if(op=='/'){ value= value / value2; }
+            }
+        }
+        return value;
+    }
+}
+
+
+$(document).ready(function()
+{
+    $('.calculator').each(function()
+    {
+        // this refers to the <div> with class calculator
+        setup_calc(this);
+    })
+})
