165f9c1186fdf7f24cdf3b5fdabbb66d8e308943
OpenShift guest <seterman@mit.edu>
2013-06-10 15:36:58 -0400

    negative number calculator

--- day1.js
+++ day1.js
@@ -2,11 +2,12 @@ function calculate(text)
 {
     var pattern = /\d+|\+|\-|\*|\/|\(|\)/g;
     var tokens = text.match(pattern);
+    if(tokens===null){ tokens = []; }
     
     try
     {
         var answer = evaluate(tokens);
-        if(tokens.length !== 0){ throw "Ill-formed exception"; }
+        if(tokens.length !== 0){ throw "Ill-formed expression"; }
         return String(answer);
     }
     catch(err)
@@ -33,24 +34,25 @@ function read_operand(tokens)
     var first = tokens[0];
     var number = parseInt(first,10);
     
-    if (first=="(")
-    {
+    if (first=="("){
         tokens.shift();
         number = evaluate(tokens);
     }
-    if (first == ")")
-    {
+    if (first == ")"){
         return ")";
     }
-    if (isNaN(number))   
-    {
-       throw "Number expected";
-    }
-    else
-    {
+    if(first== "-"){
         tokens.shift();
-        return number;
+        number = parseInt(tokens[0],10)
+        tokens.shift();
+        return -1*number;
     }
+    if (isNaN(number)){
+       throw "Number expected"; 
+    }
+    else{
+        tokens.shift();
+        return number;}
     
 }
 
@@ -58,28 +60,19 @@ function evaluate(tokens)
 {
     var operands = ['+','-','*','/'];
     
-    if (tokens.length === 0)
-    {
-        throw "Missing operand";
-    }
-    else
-    {
+    if (tokens.length === 0){
+        throw "Missing operand";}
+    else{
         var value = read_operand(tokens);
         
-        while(tokens.length !== 0)
-        {
-            if(tokens[0]==')')
-            {
+        while(tokens.length !== 0){
+            if(tokens[0]==')'){
                 tokens.shift();
-                return value;
-            }
+                return value; }
             var op = tokens.shift();
-            if(operands.indexOf(op)==-1)
-            {
-                throw "Missing operand";
-            }
-            else
-            {
+            if(operands.indexOf(op)==-1){
+                throw "Missing operand";}
+            else{
                 var value2 = read_operand(tokens);
                 if(op=='+'){ value= value + value2; }
                 if(op=='-'){ value= value - value2; }
@@ -92,10 +85,8 @@ function evaluate(tokens)
 }
 
 
-$(document).ready(function()
-{
-    $('.calculator').each(function()
-    {
+$(document).ready(function(){
+    $('.calculator').each(function(){
         // this refers to the <div> with class calculator
         setup_calc(this);
     })
