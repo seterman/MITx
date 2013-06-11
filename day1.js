7d2ab213ff63f349a15976f82e230434ffaf92c9
OpenShift guest <seterman@mit.edu>
2013-06-11 11:04:37 -0400

    stylesheet stuff

--- day1.js
+++ day1.js
@@ -21,7 +21,7 @@ function calculate(text)
 function setup_calc(div)
 {
     var input = $('<input></input>',{type:"text", size: 50});
-    var output  = $('<div></div>');
+    var output = $('<div class="output"></div>');
     var button = $('<button>Calculate</button>');
     button.bind("click", function(){
         output.text(String(calculate(input.val())));
